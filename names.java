10baa5617d5b08ee8b1b455816240494d8593802
victorytree <115655076+victtorytree@users.noreply.github.com>
2023-09-22 19:56:26 +0300

    hometask_5 for java block in GeekBrains

---
+++ names.java
@@ -0,0 +1,37 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.Random;
+
+enum Spisok {
+    Jane, Kort, Gretta, Shone, James, Robert, Kate, Jordan, Scott, Elizabeth;
+}
+
+public class names {
+    public static void main(String[] args) {
+        
+        HashMap<String, ArrayList<String>> val = new HashMap<>();
+        // Заполняем справочник рандомными номерами
+        for (int i = 0; i < 10; i++) {
+            String item = String.valueOf(Spisok.values()[new Random().nextInt(Spisok.values().length)]);
+            String number = String.valueOf(new Random().nextInt(100000,1000000));
+            ArrayList<String> phone = new ArrayList<>();
+            if(val.containsKey(item)) {
+                val.get(item).add(number);
+            } else {
+                phone.add(number);
+                val.put(item, phone);
+            }
+        }
+ 
+        ArrayList<Map.Entry<String, ArrayList<String>>> sorted = new ArrayList<>(val.entrySet());
+        sorted.sort((e1, e2) -> e2.getValue().size() - e1.getValue().size());
+
+        ArrayList<String> result = new ArrayList<>();
+        for (Map.Entry <String, ArrayList<String>> person : sorted) {
+                result.add(person.getKey() + ": " + person.getValue());
+        }
+        System.out.println(result);            
+
+    }
+}
